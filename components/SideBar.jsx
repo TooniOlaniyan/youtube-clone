40a91c1dad13c9d4322146f5fcaa88fb6223f46b
TooniOlaniyan <asalamlatif@gmail.com>
2022-10-03 21:32:06 +0100

    sidebar

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSession, signOut } from 'next-auth/react'
+import SideBarRow from './SideBarRow'
+import { HiUserGroup ,HiOutlineDesktopComputer } from 'react-icons/hi'
+import {FiUsers} from 'react-icons/fi'
+import {BiShoppingBag ,BiChevronDown} from 'react-icons/bi'
+import {AiOutlineCalendar , AiOutlineClockCircle} from 'react-icons/ai'
 
 function SideBar() {
+    const session = useSession()
   return (
-    <div>SideBar</div>
+    <div className='p-2 mt-5 max-w-[600px] xl:min-w-[300px]'>
+        <SideBarRow src={session.data.user.image} title={session.data.user.name} />
+        <SideBarRow Icon={FiUsers} title='Friends' />
+        <SideBarRow Icon={HiUserGroup} title='Groups' />
+        <SideBarRow Icon={BiShoppingBag} title='Marketplace' />
+        <SideBarRow Icon={HiOutlineDesktopComputer} title='Watch' />
+        <SideBarRow Icon={AiOutlineCalendar} title='Event' />
+        <SideBarRow Icon={AiOutlineClockCircle} title='Memories' />
+        <SideBarRow Icon={BiChevronDown} title='See More' />
+
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Header from '../components/Header'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import LogIn from '../components/LogIn'
+import SideBar from '../components/SideBar'
 
 
 export default function Home() {
@@ -22,7 +23,7 @@ export default function Home() {
       </Head>
       <Header/>
       <main>
-        {/* sidebar */}
+        <SideBar/>
         {/* feed */}
         {/* widgets */}
       </main>
