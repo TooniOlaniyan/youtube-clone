6c8dbbcd04e7b6adc01ad7c554a31261604c2041
TooniOlaniyan <asalamlatif@gmail.com>
2022-10-04 10:57:48 +0100

    input box

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import InputBox from './InputBox'
 import Stories from './Stories'
 
 function Feed() {
 
   return (
-    <div className=' h-[200vh] w-[50vw] overflow-y-scroll mx-auto'>
-       <div className='flex'>
+    <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto'>
+       <div className='mx-auto max-w-md md:max-w-lg lg:max-w-2xl'>
         <Stories/>
+        <InputBox/>
        </div>
     </div>
   )
--- components/Stories.jsx
+++ components/Stories.jsx
@@ -4,35 +4,40 @@ import StoryCard from './StoryCard'
 function Stories() {
         const stories = [
           {
+            id: 1,
             name: 'Tooni Olaniyan',
             src: 'https://links.papareact.com/zof',
             profile: 'https://links.papareact.com/l4v',
           },
           {
+            id: 2,
             name: 'Elon Musk',
-            src: 'https://links.papareact.com/zof',
-            profile: 'https://links.papareact.com/l4v',
+            src: 'https://links.papareact.com/k2j',
+            profile: 'https://links.papareact.com/f0p',
           },
           {
+            id: 3,
             name: 'Doja Cat',
-            src: 'https://links.papareact.com/zof',
-            profile: 'https://links.papareact.com/l4v',
+            src: 'https://links.papareact.com/xql',
+            profile: 'https://links.papareact.com/snf',
           },
           {
+            id: 4,
             name: 'Tywin Lanister',
             src: 'https://links.papareact.com/zof',
             profile: 'https://links.papareact.com/l4v',
           },
           {
+            id: 5,
             name: 'Amond Targaryn',
-            src: 'https://links.papareact.com/zof',
-            profile: 'https://links.papareact.com/l4v',
+            src: 'https://links.papareact.com/4u4',
+            profile: 'https://links.papareact.com/zvy',
           },
         ]
   return (
     <div className='flex mx-auto space-x-3 justify-center'>
         {stories.map((story) => (
-            <StoryCard name={story.name} src={story.src} proile={story.profile}  />
+            <StoryCard key={story.id} name={story.name} src={story.src} proile={story.profile}  />
         ) )}
 
     </div>
